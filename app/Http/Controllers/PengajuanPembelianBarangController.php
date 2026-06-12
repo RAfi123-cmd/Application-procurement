25c39faec7bffc4a8283abb1236e635ebefbf10f
Rafi123-CMD <rafiulhuda27@gmail.com>
2026-06-12 22:10:15 +0700

    create data pengajuan barang

--- app/Http/Controllers/PengajuanPembelianBarangController.php
+++ app/Http/Controllers/PengajuanPembelianBarangController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\PengajuanPembelianBarang;
+use App\Models\PengajuanPembelianBarangDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PengajuanPembelianBarangController extends Controller
 {
@@ -24,5 +26,59 @@ class PengajuanPembelianBarangController extends Controller
                 }
             })->paginate(10);
         return view('pages.pengajuan', compact('list_pengajuan'));
+    }
+
+    public function create(PengajuanPembelianBarang $pengajuan_pembelian_barang)
+    {
+       
+        $user = auth()->user();
+        if ($user->cannot('create pengajuan-pembelian-barang')) {
+            return abort(403, 'Kamu tidak memiliki hak akses ke halaman ini');
+        }
+        return view('pages.pengajuan-form-input', ['pengajuan' => $pengajuan_pembelian_barang]);
+    }
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_pengajuan' => 'required|max:255',
+            'keterangan' => 'required',
+            'nama_barang' => 'required|array|min:1',
+            'nama_barang.*' => 'required',
+            'jumlah' => 'required|array|min:1',
+            'jumlah.*' => 'required|numeric',
+            'harga_satuan' => 'required|array|min:1',
+            'harga_satuan.*' => 'required|numeric',
+            'spesifikasi' => 'required|array|min:1',
+            'spesifikasi.*' => 'required',
+        ], [
+            'nama_barang.*.required' => 'Nama barang wajib diisi',
+            'jumlah.*.required' => 'Jumlah barang wajib diisi',
+            'harga_satuan.*.required' => 'Harga satuan barang wajib diisi',
+            'spesifikasi.*.required' => 'Spesifikasi barang wajib diisi',
+        ]);
+        DB::beginTransaction();
+        try {
+            $pengajuan = PengajuanPembelianBarang::create([
+                'nomor' => numbering('pengajuan_pembelian_barang', 'PPB' . date('ym')),
+                'nama_pengajuan' => $request->nama_pengajuan,
+                'keterangan' => $request->keterangan,
+                'user_id' => auth()->user()->id
+            ]);
+            $list_barang = [];
+            foreach ($request->nama_barang as $key => $nama_barang) {
+                $list_barang[] = new PengajuanPembelianBarangDetail([
+                    'nama_barang' => $nama_barang,
+                    'spesifikasi' => $request->spesifikasi[$key],
+                    'jumlah' => $request->jumlah[$key],
+                    'harga_satuan' => $request->harga_satuan[$key]
+                ]);
+            }
+            $pengajuan->detail()->saveMany($list_barang);
+            DB::commit();
+            return redirect('pengajuan-pembelian-barang')->with('success', 'Data berhasil disimpan');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+            return redirect()->back()->with('error', $th->getMessage());
+        }
     }
 }
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,16 +1,13 @@
 <nav class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
-    <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         Dashboard
@@ -48,10 +45,9 @@
                 </div>
             </div>
 
-            <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <div class="relative">
-                    <div>
+                <x-dropdown align="right" width="48">
+                    <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
                             <div>{{ Auth::user()->name }}</div>
 
@@ -61,40 +57,37 @@
                                 </svg>
                             </div>
                         </button>
-                    </div>
-                    <div id="dropdown" class="absolute hidden z-500 mt-2 w-48 rounded-md shadow-lg ltr:origin-top-right rtl:origin-top-left end-0">
-                        <div class="rounded-md ring-1 ring-black ring-opacity-5 py-1 bg-white dark:bg-gray-700">
-                            <x-dropdown-link :href="route('profile.edit')">
-                                Profile
+                    </x-slot>
+
+                    <x-slot name="content">
+                        <x-dropdown-link :href="route('profile.edit')">
+                            Profile
+                        </x-dropdown-link>
+
+                        <form method="POST" action="{{ route('logout') }}">
+                            @csrf
+
+                            <x-dropdown-link :href="route('logout')"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                Logout
                             </x-dropdown-link>
-
-                            <!-- Authentication -->
-                            <form method="POST" action="{{ route('logout') }}">
-                                @csrf
-
-                                <x-dropdown-link :href="route('logout')"
-                                        onclick="event.preventDefault();
-                                                    this.closest('form').submit();">
-                                    Logout
-                                </x-dropdown-link>
-                            </form>
-                        </div>
-                    </div>
-                </div>
+                        </form>
+                    </x-slot>
+                </x-dropdown>
             </div>
 
-            <!-- Hamburger -->
-            <div class="-me-2 flex items-center sm:hidden">
+            <div class="-me-2 flex items-center sm:hidden" x-data="{ open: false }">
                 <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-900 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-900 focus:text-gray-500 dark:focus:text-gray-400 transition duration-150 ease-in-out">
                     <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                         <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                        <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 </button>
             </div>
         </div>
     </div>
 
-    <!-- Responsive Navigation Menu -->
     <div class="hidden sm:hidden" id="navbar-default">
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
@@ -132,7 +125,6 @@
             @endcan
         </div>
 
-        <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">{{ Auth::user()->name }}</div>
@@ -144,14 +136,13 @@
                     Profile
                 </x-responsive-nav-link>
 
-                <!-- Authentication -->
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
                     <x-responsive-nav-link :href="route('logout')"
                             onclick="event.preventDefault();
                                         this.closest('form').submit();">
-                        Logout
+                        Log Out
                     </x-responsive-nav-link>
                 </form>
             </div>
